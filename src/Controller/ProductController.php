2c954cbc6efa2b2207c9c25a57c5b9ab9ffcd20a
Karol Kijowski <kkijowski11@gmail.com>
2022-04-09 01:11:40 +0200

    deleted bootstrap, might add some new styling later

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Product Controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProductController.
+ *
+ * @Route("/")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * Product page.
+     *
+     * @Route(
+     *     "/product",
+     *     name="product",
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response HTTP Response
+     */
+    public function product_page(): Response
+    {
+        return $this->render(
+            'product/index.html.twig',
+        );
+    }
+}
