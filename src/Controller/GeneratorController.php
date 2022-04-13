a1e1c012d234df79fff4ef537916931066a74923
Karol Kijowski <kkijowski11@gmail.com>
2022-04-13 11:25:17 +0200

    gen

--- src/Controller/GeneratorController.php
+++ src/Controller/GeneratorController.php
@@ -11,9 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use ColorContrast\ColorContrast;
 use App\Service\GeneratorService;
-
-
-
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class GeneratorController.
@@ -49,7 +47,7 @@ class GeneratorController extends AbstractController
      *
      * @return \Symfony\Component\HttpFoundation\Response HTTP Response
      */
-    public function generator(): Response
+    public function generator(Request $request): Response
     {
         $input =
             [
@@ -59,28 +57,58 @@ class GeneratorController extends AbstractController
                 'ciekawość'
             ];
 
-        $contrast = new ColorContrast();
-        $contrast->addColors(0xff0000, 0x002200, 0x0022ff, 0xffffff);
-        $combinations = $contrast->getCombinations(ColorContrast::MIN_CONTRAST_AAA);
-        foreach ($combinations as $combination) {
-            printf(
-                "#%s on the Background color #%s has a contrast value of %f \n",
-                $combination->getForeground(),
-                $combination->getBackground(),
-                $combination->getContrast()
-            );
-            echo '<br>';
+        $prefix = '0x';
+        $color1 = $request->query->get('color1');
+        $color2 = $request->query->get('color2');
+
+        // $color1 = substr($color1, 1);
+        // $color1 = $prefix . $color1;
+
+        // $color2 = substr($color2, 1);
+        // $color2 = $prefix . $color2;
+
+        // echo $color1;
+        // echo '<br>';
+        // echo hex2bin($color1);
+        // echo '<br>';
+        // echo bin2hex($color1);
+
+
+        function strtohex($string)
+        {
+            $string = str_split($string);
+            foreach ($string as &$char)
+                $char = dechex(ord($char));
+            return implode('', $string);
         }
 
+        $test = strtohex($color1);
+        echo $test;
+        echo '<br>';
+        echo hex2bin($test);
+
+        // $contrast = new ColorContrast();
+        // $contrast->addColors( 0x0022ff, 0xffffff);
+        // $combinations = $contrast->getCombinations(ColorContrast::MIN_CONTRAST_AAA);
+        // foreach ($combinations as $combination) {
+        //     printf(
+        //         "#%s on the Background color #%s has a contrast value of %f \n",
+        //         $combination->getForeground(),
+        //         $combination->getBackground(),
+        //         $combination->getContrast()
+        //     );
+        //     echo '<br>';
+        // }
+
         $color = $this->generatorService->check_color($input)['color'];
+
 
 
 
         return $this->render(
             'generator/index.html.twig',
             [
-                'color' => $color,
-                'input' => $input,
+                // 'color' => $color,
             ]
         );
     }
